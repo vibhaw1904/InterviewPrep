16e43d8f8c0859ae746571481ad71511df061907
vibhaw raj <vibhawraj494@gmail.com>
2024-07-04 17:43:27 +0530

    [..]

--- uber/src/components/Shape.tsx
+++ uber/src/components/Shape.tsx
@@ -1,14 +1,24 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 type ShapeProps = {
   data: number[][];
 };
 
 const Shape: React.FC<ShapeProps> = ({ data }) => {
-
+    const [show,setShow]=useState(new Set());
 
     const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.preventDefault();
+        // e.preventDefault();
+        const{target}=e
+        const index = (target as HTMLDivElement).getAttribute('data-index');     
+        if(index===null){
+            return;
+        }
+        setShow((prev)=>{
+            const newSet = new Set(prev);
+            newSet.add(Number(index));
+            return newSet;
+        })
       };
       
 
@@ -18,8 +28,14 @@ const Shape: React.FC<ShapeProps> = ({ data }) => {
     <div className='p-12 grid grid-cols-3 items-center align-middle max-w-fit gap-5'>
       {boxes.map((box, index) => {
         const isVisible = box === 1 ? 'cursor-pointer border border-black' : 'cursor-initial opacity-0';
+        const isSelected=show.has(index);
+        const selected=isSelected?'bg-green-400 cursor-not-allowed':''
         return (
-          <div className={`w-20 h-20 ${isVisible}`} key={`${box}-${index} `} onClick={handleClick}>
+          <div  className={`w-20 h-20 ${isVisible} ${selected}`}
+          key={index}
+          data-index={index}
+          data-status={isVisible}
+          onClick={handleClick}>
             
           </div>
         );
