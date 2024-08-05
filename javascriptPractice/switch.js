67e5e7bed70e5194853ec4fc97f03bbda37c2aad
vibhaw raj <vibhawraj494@gmail.com>
2024-08-05 14:23:59 +0530

    [switchg]

---
+++ javascriptPractice/switch.js
@@ -0,0 +1,31 @@
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    console.log( 'Too small' );
+    break;
+  case 4:
+    console.log( 'Exactly!' );
+    break;
+  case 5:
+    console.log( 'Too big' );
+    break;
+  default:
+    console.log( "I don't know such values" );
+}
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert( 0 );
+    break;
+
+  case 1:
+    alert( 1 );
+    break;
+
+  case 2:
+  case 3:
+    alert( '2,3' );
+    break;
+}
