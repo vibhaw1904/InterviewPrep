3a77d04903f49232112324df0751d7f5c68e2ff5
vibhaw raj <vibhawraj494@gmail.com>
2024-08-15 13:19:48 +0530

    task3

--- javascriptPractice/funcTask.js
+++ javascriptPractice/funcTask.js
@@ -6,17 +6,27 @@
 //     }
 //   }
 ///task1
-  function checkAge(age){
-    return age>18 ?  true :  console.log
-    ('Did parents allow you?');
-  }
-  checkAge(12)
+function checkAge(age) {
+  return age > 18 ? true : console.log("Did parents allow you?");
+}
+checkAge(12);
 
 //   task2
-function min(a,b){
-    return (a>b)?console.log(b):console.log(a);
-    ;
-    
+function min(a, b) {
+  return a > b ? console.log(b) : console.log(a);
 }
-min(6,8);
-min(10,2)
+min(6, 8);
+min(10, 2);
+
+//task3
+
+function pow(x, n) {
+  let ans = x;
+  if(n==0 )ans=1;
+  for (let i = 1; i < n; i++) {
+    ans = ans * x;
+  }
+  console.log(ans);
+}
+
+pow(3, 3);
