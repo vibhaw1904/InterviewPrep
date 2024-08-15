477375457299e05213329bc9f1cc56ec0a333625
vibhaw raj <vibhawraj494@gmail.com>
2024-08-15 13:10:19 +0530

    task

---
+++ javascriptPractice/funcTask.js
@@ -0,0 +1,12 @@
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Did parents allow you?');
+//     }
+//   }
+  function checkAge(age){
+    return age>18 ?  true :  console.log
+    ('Did parents allow you?');
+  }
+  checkAge(12)
