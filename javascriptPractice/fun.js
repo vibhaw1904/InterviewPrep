be1e840c4f3f09718a62c7c882d6de49b624c167
vibhaw raj <vibhawraj494@gmail.com>
2024-08-20 23:18:20 +0530

    ..

---
+++ javascriptPractice/fun.js
@@ -0,0 +1,14 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  function showOk() {
+    alert( "You agreed." );
+  }
+  
+  function showCancel() {
+    alert( "You canceled the execution." );
+  }
+  
+  ask("Do you agree?", showOk, showCancel);
