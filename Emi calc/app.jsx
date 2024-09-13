03a33f83b3cd70570e9952a9cb716a57a43b9212
vibhaw raj <vibhawraj494@gmail.com>
2024-09-13 15:16:18 +0530

    fst chckPoint

--- Emi calc/app.jsx
+++ Emi calc/app.jsx
@@ -8,9 +8,49 @@ export default function App() {
   const [downPayement, setDownPayment] = useState(0);
   const [tenure, setTenure] = useState(12);
   const [emi, setEmi] = useState(0);
+  const updateEmi = () => {};
+  const updatedownPayment = () => {};
+
   return (
     <div className="App">
-      <div></div>
+      <span className="titleh"> Emi cal</span>
+      <span className="title">Total cost</span>
+      <input
+        type="number"
+        value={totoalCost}
+        onChange={(e) => setTotalCost(e.target.value)}
+        placeholder="total cost here"
+      />
+      <span className="title">Interest rate in (%)</span>
+      <input
+        type="number"
+        value={interestRate}
+        onChange={(e) => setInterestRate(e.target.value)}
+      />
+      <span className="title">Processing fee</span>
+      <input
+        type="number"
+        value={processingfee}
+        onChange={(e) => setProcessingFee(e.target.value)}
+      />
+      <span className="title">Downpayment</span>
+      <input
+        type="range"
+        min={0}
+        max={totoalCost}
+        value={downPayement}
+        onChange={updateEmi}
+      />
+      <span className="title">Loan per month</span>
+      <input
+        type="range"
+        min={0}
+        max={totoalCost}
+        value={emi}
+        onChange={updatedownPayment}
+      />
+
+      <span className="title">Tenure</span>
     </div>
   );
 }
