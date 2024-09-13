af34fc6447fbdae65e6fc8e210f6f17b373bd6b6
vibhaw raj <vibhawraj494@gmail.com>
2024-09-13 15:35:14 +0530

    ui:done

--- Emi calc/app.jsx
+++ Emi calc/app.jsx
@@ -10,7 +10,8 @@ export default function App() {
   const [emi, setEmi] = useState(0);
   const updateEmi = () => {};
   const updatedownPayment = () => {};
-
+  const calculateEmi = () => {};
+  const calculateDownPayement = () => {};
   return (
     <div className="App">
       <span className="titleh"> Emi cal</span>
@@ -34,22 +35,38 @@ export default function App() {
         onChange={(e) => setProcessingFee(e.target.value)}
       />
       <span className="title">Downpayment</span>
-      <input
-        type="range"
-        min={0}
-        max={totoalCost}
-        value={downPayement}
-        onChange={updateEmi}
-      />
-      <span className="title">Loan per month</span>
-      <input
-        type="range"
-        min={0}
-        max={totoalCost}
-        value={emi}
-        onChange={updatedownPayment}
-      />
+      <div>
+        <input
+          type="range"
+          min={calculateEmi(totoalCost)}
+          max={calculateEmi(0)}
+          className="slider"
+          value={downPayement}
+          onChange={updateEmi}
+        />
+        <div className="lables">
+          <label>0%</label>
+          <b>{downPayement}</b>
+          <label>100%</label>
+        </div>
+      </div>
 
+      <span className="title">Loan per month</span>
+      <div>
+        <input
+          type="range"
+          min={calculateDownPayement(totoalCost)}
+          max={calculateDownPayement(0)}
+          className="slider"
+          value={emi}
+          onChange={updatedownPayment}
+        />
+        <div className="lables">
+          <label>{calculateEmi(totoalCost)}</label>
+          <b>{downPayement}</b>
+          <label>{calculateEmi(0)}</label>
+        </div>
+      </div>
       <span className="title">Tenure</span>
     </div>
   );
